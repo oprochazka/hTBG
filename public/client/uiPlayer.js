fccb4b57acce8296a3eaa64e584c562f25609d70
Ondřej Procházka <oprochazka1@gmail.com>
2017-12-10 20:41:24 +0100

    faster 100x

--- public/client/uiPlayer.js
+++ public/client/uiPlayer.js
@@ -32,12 +32,11 @@ UIPlayer = {
 		UIDynamic.setSelectedObject(object);
 	},
 
-	refreshUi: function(player)
+	refreshPlayers: function()
 	{
-		this.nickName.innerHTML = player.name;
-		this.players.innerHTML = "";
-
 		var players = GameEngine.gameManager.getPlayers();
+
+		this.players.innerHTML = "";
 
 		for(var i = 0; i < players.length; i++)
 		{
@@ -45,20 +44,30 @@ UIPlayer = {
 
 			this.players.innerHTML = this.players.innerHTML + " <div style='color: "+ obj.color +";' > " + i +" : " + obj.name + "</div>";	
 		}
-		
-		this.building.innerHTML = player.building.length;
-		this.army.innerHTML = player.army.length;
-		this.yourTurn.style = "display: none";
+	},
+
+	refreshTurn: function()
+	{
+
+	},
+
+	refreshUi: function(player)
+	{
+		//this.nickName.innerHTML = player.name;
+
+		//this.building.innerHTML = player.building.length;
+		//this.army.innerHTML = player.army.length;
+		//this.yourTurn.style = "display: none";
 
 		if(GameEngine.gameManager.turnPlayer)
 		{
-			this.playerTurn.innerHTML =  "<span>" + GameEngine.gameManager.turnPlayer.name + "</span>";				
+		//	this.playerTurn.innerHTML =  "<span>" + GameEngine.gameManager.turnPlayer.name + "</span>";				
 		}
 		if(GameEngine.gameManager.turnPlayer == GameEngine.gameManager.getControllPlayer())
 		{
-			this.yourTurn.style = "display: block";
+		//	this.yourTurn.style = "display: block";
 		}
 
-		this.playerGold.innerHTML =  GameEngine.gameManager.getControllPlayer().gold;	
+		//this.playerGold.innerHTML =  GameEngine.gameManager.getControllPlayer().gold;	
 	}
 };
